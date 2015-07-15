e32096082adc0e34156e0b3bf78d044fcea27201
Nerio Navea <neriojose5@gmail.com>
2015-07-14 22:22:45 -0430

    SMS remember fix

--- app/models/remember.rb
+++ app/models/remember.rb
@@ -4,15 +4,17 @@ class Remember
 	end
 	def schedule()
 		schedule = Rufus::Scheduler.new
-		schedule.every '1s' do
-			remember_customer(Service.all(:next_change => Time.now.strftime("%Y-%m-%d"), :remembered => false))
+		schedule.every '10s' do
+			send_customer_remembers()
 		end
 	end
-	def remember_customer(services)
+	def send_customer_remembers()
+		services = Service.all()
 		services.each do |service|
-			@sms = send_remember(services.custmer, service)
-			puts 'SMS sended to ' + service.customer.to_s + ' by remember'
-			service.update(:remembered => true)
+			if service.next_change.to_s == Time.now.strftime("%Y-%m-%d") && service.remembered == false
+				@sms.send_remember(service.customer, service)
+				service.update(:remembered => true)
+			end
 		end
 	end
 end
